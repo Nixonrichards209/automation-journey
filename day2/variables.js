44a9a5ef3f463946de4372e5436995f9b3cd310f
Nixon <nic.ric209@gmail.com>
2026-03-14 23:01:43 +0530

    Day 2: JS Variables Exercises

---
+++ day2/variables.js
@@ -0,0 +1,101 @@
+/* Exercise  1
+
+Create three variables and store:
+
+1. your name
+
+2. your current job role
+
+3. the country you want to work in
+
+Print all three values.
+
+*/
+
+let myName  = "Nixon Richards James";
+let jobDesc = "Software QA Engineer";
+let desiredCountry = "Germany";
+
+
+console.log(myName);
+console.log(jobDesc);
+console.log(desiredCountry);
+
+
+/* Exercise # 2
+
+Exercise 2
+
+Create variables for:
+
+your age
+
+your years of experience
+
+your daily study hours
+
+Print them with clear labels in the console.
+
+*/
+
+let age = "36";
+let experience = "6 Years";
+let studyhours = "3 hrs";
+
+console.log("Age is", age);
+console.log("Experience is" , experience);
+console.log("Study Hours is ", studyhours);
+
+
+/* Exercise # 3 
+
+Create two variables:
+
+firstName
+
+lastName
+
+Print them together in one line as a full name.
+
+*/
+
+let firstName = "Nixon";
+let lastName = "James";
+
+console.log("Full Name is" ,firstName+" "+lastName);
+
+/*Exercise # 4
+
+Exercise 4
+
+Create variables for:
+
+currentSalary
+
+expectedSalary
+
+Print both values.
+
+*/
+
+let currentSalary = "INR 11,05,000 Per Annum";
+let expectedSalary = "INR 17,00,000 Per Annum";
+
+console.log(`The current salary is ${currentSalary}`);
+console.log(`The expected salary is ${expectedSalary}`);
+
+
+/* Exercise 5
+
+Create a variable called:
+
+learningGoal
+
+Store your career goal (for example: working abroad as automation QA).
+
+Print it.
+*/
+
+let learningGoal = "working abroad as automation QA";
+
+console.log(`My learning goal is ${learningGoal}`);

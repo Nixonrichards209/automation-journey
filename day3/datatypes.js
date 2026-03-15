4df280eea09465e1dabd0a2c5c3f584df25747d8
Nixon <nic.ric209@gmail.com>
2026-03-16 00:12:08 +0530

    Learning Operators

---
+++ day3/datatypes.js
@@ -0,0 +1,46 @@
+let userName  = "username";
+let password  = "password";
+let website   = "www.learnjs.com";
+
+console.log(`Username : ${userName}`);
+console.log(`Password : ${password}`);
+console.log(`Website  : ${website}`);
+
+
+/* Exercise # 2
+
+Create variables:
+age
+experience
+dailyPracticeHours
+
+*/
+
+let age = 36;
+let experience = 6;
+let practiceHours = 3;
+
+console.log(`Age : ${age}`);
+console.log(`Experience : ${experience}`);
+console.log(`Daily Practice Hours : ${practiceHours}`);
+
+
+/* Exercise # 3
+
+Create these variables:
+
+isLoggedIn
+isAutomationTester
+isLearningDaily
+
+*/
+
+let isLoggedIn = true;
+let isAutomationTester = true;
+let isLearningDaily = true;
+let isLearningGap = false;
+
+console.log(`Is Logged In :  ${isLoggedIn}`);
+console.log(`Is Automation Tester : ${isAutomationTester}`);
+console.log(`Is Learning Daily : ${isLearningDaily}`);
+console.log(`Is Learning Gap : ${isLearningGap}`);
---
+++ day3/operators.js
@@ -0,0 +1,34 @@
+let a = 10;
+let b = 5;
+
+console.log("Addition : " + " " + (a+b));
+console.log("Subtraction : " + " " + (a-b));
+console.log("Multiplication : " + " " + (a*b));
+console.log("division : " + " " + (a/b));
+
+/* Exercise # 2
+
+Print the result of these comparisons:
+
+age > 30
+experience >= 5
+age === 36
+experience < 3
+*/
+
+
+let age = 30;
+let experience = 6;
+
+console.log(age>30);
+console.log(experience >=5);
+console.log(age===36);
+console.log(experience<3);
+
+// Exercise # 4
+
+let x = 10;
+let y = "10";
+
+console.log(x==y);
+console.log(x===y);
